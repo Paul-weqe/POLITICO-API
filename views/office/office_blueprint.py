320646b684d5b08e24f9eb894cb2998903ae5850
Paul-weqe <paul1tw1@gmail.com>
2019-02-04 19:12:37 +0300

    [starts 163682052] creating blueprint for office and add '/offices' POST route

---
+++ views/office/office_blueprint.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, jsonify, request, make_response
+from politico_data import political_parties, users, offices, candidates, votes
+from random import randint
+
+office_blueprint = Blueprint('office_blueprint', __name__)
+
+@office_blueprint.route("/offices", methods=['POST'])
+def createOffice():
+    json_data = request.get_json(force=True)
+
+    required_fields = ["office_type", "office_name"]
+    for field in required_fields:
+        if field not in json_data:
+            return make_response(jsonify({
+                "status": 404, 
+                "error": "Field {} is mandatory in the request body".format(field)
+            }), 404)
+
+    office_type = json_data["office_type"]
+    office_name = json_data["office_name"]
+    office_id = randint(1, 100)
+    new_office = {
+        "id": office_id, "name": office_name, "type": office_type
+    }
+    offices.append(new_office)
+
+    return make_response(jsonify({
+        "status": 200,
+        "data": [
+            new_office
+        ]
+    }), 200)
+    
