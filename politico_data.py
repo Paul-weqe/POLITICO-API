4d6594c8acaed9d5520a6d61f5d4fe1340cba75a
Paul-weqe <paul1tw1@gmail.com>
2019-02-04 18:51:48 +0300

    adding information on the politico_data.py

--- politico_data.py
+++ politico_data.py
@@ -1,51 +1,62 @@
 political_parties = [
     {
-        'partyId': 1,
-        'partyName': 'Party 1',
-        'partyMotto': 'Party 1 motto',
+        "id": 1, "name": "Party 1", "hqAddress": "Nairobi", "logoUrl": "https://martialartsworldnews.com/wp-content/uploads/2015/11/1.jpg", "motto": "We are one", "members": 2000,
     },
     {
-        'partyId': 2,
-        'partyName': 'Party 2',
-        'partyMotto': 'Party 2 motto',
+        "id": 2, "name": "Party 2", "hqAddress": "Mombasa", "logoUrl": "https://www.michels.ca/ckfinder/userfiles/images/number2tm.png", "motto": "we are two", "members": 2500,
     },
     {
-        'partyId': 3,
-        'partyName': 'Party 3',
-        'partyMotto': 'Party 3 motto'
-    },
-    {
-        'partyId': 4,
-        'partyName': 'Party 4',
-        'partyMotto': 'Party 4 motto'
-    },
-    {
-        'partyId': 6,
-        'partyName': 'Party 6',
-        'partyMotto': 'Party 6 motto'
+        "id": 3, "name": "Party 3", "hqAddress": "Kisumu", "logoUrl", "https://vignette.wikia.nocookie.net/opartshunter/images/7/79/3.jpg/revision/latest?cb=20130603053056", "motto", "we are three", "members": 3000,
     }
 ]
 
-government_offices = [
+users = [
     {
-        'officeId': 1,
-        'officeName': 'President',
-        'officeFunction': 'Lead the country',
+        "id": 1, "firstName": "Paul", "lastName": "Waswa", "otherName": "weqe", "email": "paul@paul.com", "isAdmin": False, "phoneNumber": "0701000302010", "passportUrl": "",
     },
     {
-        'officeId': 10,
-        'officeName': 'Government',
-        'officeFunction': 'Lead the county'
+        "id": 2, "firstName": "Gideon", "lastName": "Smith", "otherName": "Salah", "email": "gidi@gidi.com", "isAdmin": False, "phoneNumber": "0710270374", "passportUrl": "",
     },
     {
-        'officeId': 35,
-        'officeName': 'MP',
-        'officeFunction': 'Head of the constituency'
+        "id": 3, "firstName", "Peter", "lastName": "Kenyatta", "otherName": "lil", "email": "peter@peter.com", "isAdmin": True, "phoneNumber": "07374757282", "passportUrl": "",
     },
     {
-        'officeId': 67,
-        'officeName': 'DC',
-        'officeFunction': 'Head of the district'
+        "id": 4, "firstName": "Brian", "lastName": "Kiasi", "otherName": "Ule msee", "email": "brian@brian.com", "isAdmin": True, "phoneNumber": "0712873873", "passportUrl": "",
     }
 ]
 
+offices = [
+    {
+        "id": 1, "type": "local_government", "name": "chief",
+    },
+    {
+        "id": 2, "type": "legislative", "name": "MP"
+    },
+    {
+        "id": 3, "type": "state", "name": "president"
+    },
+    {
+        "id": 4, "type": "federal", "name": "magistrate"
+    }
+]
+
+candidates = [
+    {
+        "id": 1, "office": 2, "party": 1, "candidate": 1, 
+    },
+    {
+        "id": 2, "office": 4, "party": 3, "candidate": 3
+    },
+    {
+        "id": 3, "office": 1, "party": 2, "candidate": 2,
+    }
+]
+
+votes = [
+    {
+        "id": 1, "candidateOn": 1, "createdBy": 1, "office": 1, "body": "",
+    },
+    {
+        "id": 2, "candidateOn": 2, "createdBy": 2, "office": 2, "body": "",
+    }
+]
