48875ae6aa1077d1a0e5f5e986163e6114176a7e
Paul-weqe <paul1tw1@gmail.com>
2019-02-07 18:37:57 +0300

    [develop branch] refactoring code in politico_api/config.py

--- politico_api/config.py
+++ politico_api/config.py
@@ -7,18 +7,13 @@ app = Flask(__name__)
 app.register_blueprint(office_blueprint_v1)
 app.register_blueprint(party_blueprint_v1)
 
+def handle_error(error_number, error_message):
+    @app.errorhandler(error_number)
+    def page_not_found(e):
+        return make_response(jsonify({
+            "status": error_number,
+            "error": error_message
+        }), erorr_number)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return make_response(jsonify({
-        "status": 404,
-        "error": "The page you are looking for could not be found"
-    }), 404)
-
-
-@app.errorhandler(405)
-def method_not_allowed(e):
-    return make_response(jsonify({
-        "status": 405,
-        "error": "That method cannot be used on this route"
-    }), 405)
+handle_error(404, "The page you are looking for could not be found")
+handle_error(405, "That method cannot be used in this route")
